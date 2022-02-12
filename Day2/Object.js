8596eddd8c264b9b9944888e3a35dfa94db9e4f8
Himanshu <technophilic000@gmail.com>
2022-02-12 09:53:48 +0800

    Second Commit

--- Day2/Object.js
+++ Day2/Object.js
@@ -4,7 +4,18 @@ let person = {
     firstName :"Himanshu",
     lastName: "Singh",
     college: "Shivalik College Of Engineering ",
-    course: "Btech"
+    course: "Btech",
+    // sayHi : function (){
+    //     console.log("Hello");
+
+    // }
+
+    sayHi (){
+        console.log("Hello");
+    }
+
+
+   
 };
 
 // console.log(person.college);
@@ -14,6 +25,33 @@ person.age = 19;
 
 // console.log('height' in person);
 
-for(keys in person){
-    console.log(keys);
-}
+// for(let keys in person){
+//     console.log(keys);
+// }
+
+// for(let key in person){
+//     console.log(person[key]);
+// }
+
+//   1st Method
+
+// person.sayHi = function (){
+//     console.log("Hello");
+// }
+
+// person.sayHi();
+
+//  2nd Method
+
+
+// function greet(){
+//     console.log("Hello");
+// }
+
+// person.sayHi = greet;
+
+// person.sayHi();
+
+//  3rd Method
+
+// person.sayHi();
