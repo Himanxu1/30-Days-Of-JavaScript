33239c34d85d5538847dc25ad71ac7fbf305fa31
Himanshu <technophilic000@gmail.com>
2022-03-07 08:12:14 +0530

    Day10

---
+++ Day10/Map.js
@@ -0,0 +1,24 @@
+//  Map function is used when we want to perform opertaion on every element of the array
+
+//  Previously
+
+let arr = [2,3,4,5];
+
+// let arr1= [];
+
+// for(let i=0;i<arr.length;i++){
+//     arr1[i] = arr[i] * 2;
+// }
+
+// console.log(arr1);
+
+//  Anonymous Function
+// let arr1 = arr.map(function(val){
+//       return val * 3;
+// })
+
+//  Arrow function
+
+let arr1 = arr.map((val) => val *4);
+
+console.log(arr1);
---
+++ Day10/reduce.js
@@ -0,0 +1,15 @@
+//    Reduce Function in JavaScript
+
+let arr = [1,3,-1,2];
+
+// let sum=0;
+// for(let n of arr)
+//      sum += n;
+
+//    console.log(sum);  
+
+const sum = arr.reduce((accumulator,currentValue) => {
+    return accumulator += currentValue;
+},0);
+
+console.log(sum);
